328ab0c717620380b8ae49fc0b583a50f350acb2
RGBKnights <master@rgbknights.com>
2022-06-10 22:56:18 -0400

    Imported project

---
+++ src/Model.cs
@@ -0,0 +1,120 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HGV.Stormcrafter
+{
+    public class Hero
+    {
+        [JsonProperty("id")]
+        public int Id { get; set;} // Partition key
+
+        [JsonProperty("key")]
+        public string Key { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("aliases")]
+        public List<string> Aliases { get; set; } = new List<string>();
+
+        [JsonProperty("image_banner")]
+        public string ImageBanner { get; set; }
+
+        [JsonProperty("image_icon")]
+        public string ImageIcon { get; set; }
+
+        [JsonProperty("image_profile")]
+        public string ImageProfile { get; set; }
+
+        [JsonProperty("image_portrait")]
+        public string ImagePortrait { get; set; }
+
+        [JsonProperty("ability_replace_required")]
+        public bool AbilityReplaceRequired { get; set; }
+
+        [JsonProperty("strength_gain")]
+        public double StrengthGain { get; set; }
+
+        [JsonProperty("max_strength")]
+        public double MaxStrength { get; set; }
+
+        [JsonProperty("intelligence_gain")]
+        public double IntelligenceGain { get; set; }
+
+        [JsonProperty("max_intelligence")]
+        public double MaxIntelligence { get; set; }
+
+        [JsonProperty("agility_gain")]
+        public double AgilityGain { get; set; }
+
+        [JsonProperty("max_agility")]
+        public double MaxAgility { get; set; }
+
+        [JsonProperty("primary_attribute")]
+        public string AttributePrimary { get; set; }
+
+        [JsonProperty("attack_capabilities")]
+        public string AttackCapabilities { get; set; }
+
+        [JsonProperty("win_rate")]
+        public double WinRate { get; set; }
+
+        [JsonProperty("picked")]
+        public bool Picked { get; set; }
+
+        [JsonProperty("damage")]
+        public int Damage { get; set; }
+
+        [JsonProperty("roles")]
+        public List<string> Roles { get; set; }
+
+        [JsonProperty("complexity")]
+        public int Complexity { get; set; }
+
+        
+    }
+
+    public class Patches
+    {
+        [JsonProperty("overall")]
+        public List<string> Overall { get; set; }
+
+        [JsonProperty("majorCounts")]
+        public List<int> MajorCounts { get; set; }
+
+        [JsonProperty("majors")]
+        public List<string> Majors { get; set; }
+
+        [JsonProperty("majorsMinors")]
+        public Dictionary<string, List<string>> Details { get; set; }
+    }
+
+    public class HeroData
+    {
+        [JsonProperty("wins")]
+        public int Wins { get; set; }
+
+        [JsonProperty("numGames")]
+        public int Total { get; set; }
+
+        [JsonProperty("winrate")]
+        public double WinRate { get; set; }
+    }
+
+    public class Data
+    {
+        [JsonProperty("patches")]
+        public Patches Patches { get; set; }
+
+        [JsonProperty("heroStats")]
+        public Dictionary<int, Dictionary<string, HeroData>> Heroes { get; set;}
+    }
+
+    public class Root
+    {
+        [JsonProperty("data")]
+        public Data Data { get; set; }
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,48 @@
+﻿using HGV.Basilius.Client;
+using HGV.Stormcrafter;
+using Newtonsoft.Json;
+
+var httpClient = new HttpClient();
+var metaClient = new MetaClient();
+var statsUrl = "https://windrun.io/api/heroes";
+
+var response = await httpClient.GetStringAsync(statsUrl);
+var result = JsonConvert.DeserializeObject<Root>(response);
+var major = result.Data.Patches.Majors.Last();
+var patch = "7.31";
+var fallback = "7.30";
+var stats = result.Data.Heroes.Select(_ => new { Id = _.Key, Data = _.Value.ContainsKey(patch) ? _.Value[patch] : _.Value[fallback] }).ToList();
+var meta = metaClient.GetHeroes().Where(_ => _.AbilityDraftEnabled == true).ToList();
+
+var collection = meta
+    .Join(stats, _ => _.Id, _ => _.Id, (lhs, rhs) => new Hero()
+    {
+        Id = lhs.Id,
+        Name = lhs.Name,
+        Key = lhs.Key.Replace("npc_dota_hero_", ""),
+        Aliases = lhs.NameAliases,
+        ImageBanner = lhs.ImageBanner,
+        ImageIcon = lhs.ImageIcon,
+        ImageProfile = lhs.ImageProfile,
+        ImagePortrait = lhs.ImagePortrait,
+        AbilityReplaceRequired = lhs.AbilityReplaceRequired,
+        StrengthGain = lhs.AttributeStrengthGain,
+        MaxStrength = lhs.AttributeBaseStrength + (lhs.AttributeStrengthGain * 30),
+        IntelligenceGain = lhs.AttributeIntelligenceGain,
+        MaxIntelligence = lhs.AttributeBaseIntelligence + (lhs.AttributeIntelligenceGain * 30),
+        AgilityGain = lhs.AttributeAgilityGain,
+        MaxAgility = lhs.AttributeBaseAgility + (lhs.AttributeAgilityGain * 30),
+        AttributePrimary = lhs.AttributePrimary,
+        AttackCapabilities = lhs.AttackCapabilities,
+        Damage = (lhs.AttackDamageMin + lhs.AttackDamageMax) / 2,
+        Roles = lhs.Roles,
+        WinRate = rhs.Data.WinRate,
+        Complexity = lhs.Complexity,
+        Picked = false,
+    })
+    .OrderBy(_ => _.Name)
+    .ToList();
+
+
+var json = JsonConvert.SerializeObject(collection, Formatting.Indented);
+await File.WriteAllTextAsync("heroes.json", json);
