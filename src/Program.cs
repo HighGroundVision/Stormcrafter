ff754b2f15c34f834f6fea6f9adf9d1c2e830364
RGBKnights <master@rgbknights.com>
2023-09-27 11:42:58 -0400

    patch changes?

--- src/Program.cs
+++ src/Program.cs
@@ -8,9 +8,9 @@ var statsUrl = "https://windrun.io/api/heroes";
 
 var response = await httpClient.GetStringAsync(statsUrl);
 var result = JsonConvert.DeserializeObject<Root>(response);
-var major = result.Data.Patches.Majors.Last();
-var patch = "7.31";
-var fallback = "7.30";
+// var major = result.Data.Patches.Majors.Last();
+var patch = "7.33";
+var fallback = "7.33";
 var stats = result.Data.Heroes.Select(_ => new { Id = _.Key, Data = _.Value.ContainsKey(patch) ? _.Value[patch] : _.Value[fallback] }).ToList();
 var meta = metaClient.GetHeroes().Where(_ => _.AbilityDraftEnabled == true).ToList();
 
